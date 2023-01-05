d11e000fab85e29881001de1b3366f75f3592e0b
Judin <dafajudin@student.telkomuniversity.ac.id>
2023-01-05 22:18:54 +0700

    upload all files to github

---
+++ 2.py
@@ -0,0 +1,5 @@
+#create tuple
+t = (10, 20, 30, 40, 50)
+
+#find index of 30
+print(t.index(30))
---
+++ 3.py
@@ -0,0 +1,7 @@
+#create dictionary
+dict1 = {'a': 1, 'b': 2, 'c': 3}
+dict2 = {'a': 1, 'b': 2, 'c': 3}
+
+#print dictionary
+print(dict1)
+print(dict2)
